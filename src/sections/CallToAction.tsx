194be5fffc54fa4bea0924b67fc1beac8123f914
NatnaelDemelash <natidemelash074@gmail.com>
2024-11-27 21:33:12 +0300

    site footer

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -5,7 +5,7 @@ export default function CallToAction() {
         <div className="flex flex-none gap-16 text-7xl font-medium">
           {Array.from({ length: 10 }).map((_, i) => (
             <div key={i} className="flex gap-16 items-center">
-              <span className="text-lime-400">&#10038;</span>
+              <span className="text-lime-400 text-7xl">&#10038;</span>
               <span>Try it for free</span>
             </div>
           ))}
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,32 @@
+import Image from 'next/image';
+
+import logoImage from '@/assets/images/logo.svg';
+
 const footerLinks = [
-    { href: "#", label: "Contact" },
-    { href: "#", label: "Privacy Policy" },
-    { href: "#", label: "Terms & Conditions" },
+  { href: '#', label: 'Contact' },
+  { href: '#', label: 'Privacy Policy' },
+  { href: '#', label: 'Terms & Conditions' },
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+  return (
+    <section className="py-16">
+      <div className="container">
+        <div className="flex flex-col md:flex-row md:justify-between items-center gap-6">
+          <div>
+            <Image src={logoImage} alt="Logo image" />
+          </div>
+          <div>
+            <nav className="flex gap-6">
+              {footerLinks.map((link) => (
+                <a key={link.href} className="text-white/50 text-sm">
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
